dee1dbd08d140f0bfa114642d9a294d7c69321f6
Jakob <lyngsoejakob@gmail.com>
2025-01-15 07:23:03 +0100

    Added Dictionary
    Dictionary found to be the current second fastest besides BaseLineCollection

---
+++ ArrayListCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace CollectionTester
+{
+    public class ArrayListCollection<T> : BaseCollection<T>
+    {
+        private ArrayList arraylist = new ArrayList();
+
+        protected override void FillCollectionInternal(string[] input, Func<string, T> func)
+        {
+            foreach (var line in input)
+            {
+                arraylist.Add(func(line));
+            }
+        }
+
+        protected override void SortCollectionInternal(Func<T, T> comparer)
+        {
+            arraylist.Sort();
+        }
+
+        protected override void PrintCollectionInternal(TextWriter writer)
+        {
+            foreach (var line in arraylist)
+            {
+                writer.WriteLine(line);
+            }
+        }
+
+        public override T FirstObject()
+        {
+            return (T)arraylist[0];
+        }
+
+        public override T LastObject()
+        {
+            return (T)arraylist[arraylist.Count - 1];
+        }
+
+        public override int Count()
+        {
+            return arraylist.Count;
+        }
+    }
+}
---
+++ DictionaryCollection.cs
@@ -0,0 +1,44 @@
+﻿namespace CollectionTester;
+
+public class DictionaryCollection<T> : BaseCollection<T>
+{
+    private Dictionary<int, T> _dictionary = new();
+    private int _currentIndex = 0;
+
+    protected override void FillCollectionInternal(string[] input, Func<string, T> func)
+    {
+        foreach (var line in input)
+        {
+            _dictionary[_currentIndex++] = func(line);
+        }
+    }
+
+    protected override void SortCollectionInternal(Func<T, T> comparer)
+    {
+        var SortEnumerable = _dictionary.OrderBy(kvp => comparer(kvp.Value));
+        _dictionary = SortEnumerable.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+    }
+
+    protected override void PrintCollectionInternal(TextWriter writer)
+    {
+        foreach (var kvp in _dictionary)
+        {
+            writer.WriteLine(kvp.Value);
+        }
+    }
+
+    public override T FirstObject()
+    {
+        return _dictionary.Values.First();
+    }
+
+    public override T LastObject()
+    {
+        return _dictionary.Values.Last();
+    }
+
+    public override int Count()
+    {
+        return _dictionary.Count;
+    }
+}
